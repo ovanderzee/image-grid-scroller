fdcebdca6f91c4f0379e3977caad4683adf682a9
ovanderzee <hastalavista@dds.nl>
2021-03-02 21:55:56 +0100

    find the query

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h1>Image Grid Scroller</h1>
+  <h2>door Onno van der Zee</h2>
+  <TenThousand />
+  <section style="display: none">
+  	<a class="app_link" href="/services/apps/72157718495764908/">ten-thousand-images</a>
+  	<p>
+  		<strong>Key:</strong><br>
+  		<span class="api-key-info">980b06a4493f051d32e73dc5af2f56bc</span>
+  	</p>
+  	<p>
+  		<strong>Secret:</strong><br>
+  		<span class="api-key-info">1092b4b4de32d7ce</span>
+  	</p>
+  	<p id="ag-done-action-links">
+  		<a href="/services/apps/72157718495764908/">Edit app details</a>
+  		<span>-</span>
+  		<a href="/services/apps/72157718495764908/auth">Edit auth flow for this app</a>
+  		<span>-</span>
+  		<a href="/services/apps/by/me">View all Apps by You</a>
+  	</p>
+  </section>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import TenThousand from "./components/TenThousand.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    TenThousand
   }
 };
 </script>
@@ -19,8 +38,9 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  font-size: 12px;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 1.5em;
 }
 </style>
---
+++ src/components/TenThousand.vue
@@ -0,0 +1,14 @@
+<template>
+TenThousand
+</template>
+
+<script>
+export default {
+  name: "TenThousand",
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
